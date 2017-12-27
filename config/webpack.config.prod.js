56f2ce5d0c10d188d9a8168658c8f822a9e2946c
Dimitri Mikadze <dimitrimikadze@gmail.com>
2017-12-27 16:14:52 +0400

    Rename output css file to index.css, instead of hashed one

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -31,7 +31,7 @@ if (env.stringified['process.env'].NODE_ENV !== '"production"') {
 }
 
 // Note: defined here because it will be used more than once.
-const cssFilename = 'static/css/[name].[contenthash:8].css';
+const cssFilename = 'static/css/index.css';
 
 // ExtractTextPlugin expects the build output to be flat.
 // (See https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/27)
